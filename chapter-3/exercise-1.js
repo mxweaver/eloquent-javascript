94746f83423a1ab55997a0636027322dc12506d3
Maya Vera <maya@mayavera.me>
2019-02-19 20:21:10 -0500

    completed chapter 3, exercise 1

---
+++ chapter-3/exercise-1.js
@@ -0,0 +1,13 @@
+function min(a, b) {
+  if (a < b) {
+    return a
+  } else {
+    return b
+  }
+}
+
+console.log(`min(1, 2) = ${min(1, 2)}`)
+console.log(`min(0, 23) = ${min(0, 23)}`)
+console.log(`min(100, 32) = ${min(100, 32)}`)
+console.log(`min(-1, 2) = ${min(-1, 2)}`)
+console.log(`min(0, 0) = ${min(0, 0)}`)
