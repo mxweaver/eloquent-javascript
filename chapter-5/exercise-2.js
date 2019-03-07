09c8b4dec9926515f7a7e5cf357b6247701fd887
Maya Vera <maya@mayavera.me>
2019-03-06 21:32:07 -0500

    completed chapter 5 exercise 2

---
+++ chapter-5/exercise-2.js
@@ -0,0 +1,17 @@
+function loop (initial, test, update, body) {
+  let value = initial
+
+  while (test(value)) {
+    body(value)
+    value = update(value)
+  }
+}
+
+console.log('--- test 1 ---')
+loop(1, i => i < 10, i => i + 1, console.log)
+
+console.log('--- test 2 ---')
+loop(1, i => i < 1, i => i + 1, console.log)
+
+console.log('--- test 3 ---')
+loop(1, i => i < 2, i => i + 1, console.log)
