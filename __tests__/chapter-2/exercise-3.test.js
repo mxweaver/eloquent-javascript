8ae7cb34179c2136cdbda0451219241365227e03
Maya Vera <maya@mayavera.me>
2019-12-01 14:36:19 -0500

    cleaned up unit tests for chapter 3, exercise 2

--- __tests__/chapter-2/exercise-3.test.js
+++ __tests__/chapter-2/exercise-3.test.js
@@ -1,5 +1,68 @@
 const { printGrid } = require('../../chapter-2/exercise-3');
 
+const data = [
+  {
+    size: 1,
+    expected: ' ',
+  },
+  {
+    size: 2,
+    expected: ` #
+# `,
+  },
+  {
+    size: 3,
+    expected: ` # 
+# #
+ # `,
+  },
+  {
+    size: 4,
+    expected: ` # #
+# # 
+ # #
+# # `,
+  },
+  {
+    size: 5,
+    expected: ` # # 
+# # #
+ # # 
+# # #
+ # # `,
+  },
+  {
+    size: 6,
+    expected: ` # # #
+# # # 
+ # # #
+# # # 
+ # # #
+# # # `,
+  },
+  {
+    size: 7,
+    expected: ` # # # 
+# # # #
+ # # # 
+# # # #
+ # # # 
+# # # #
+ # # # `,
+  },
+  {
+    size: 8,
+    expected: ` # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # `,
+  },
+];
+
 describe('printGrid', () => {
   it('prints an 8x8 grid by default', () => {
     expect.assertions(2);
@@ -10,118 +73,20 @@ describe('printGrid', () => {
     printGrid();
 
     expect(console.log).toHaveBeenCalledTimes(1);
-    expect(console.log).toHaveBeenCalledWith(` # # # #
-# # # # 
- # # # #
-# # # # 
- # # # #
-# # # # 
- # # # #
-# # # # `);
+    expect(console.log).toHaveBeenCalledWith(data.find(({ size }) => size === 8).expected);
   });
 
-  it('prints a 1x1 grid when size = 1', () => {
-    expect.assertions(2);
+  data.forEach(({ size, expected }) => {
+    it(`prints a ${size}x${size} grid when size = ${size}`, () => {
+      expect.assertions(2);
 
-    jest.spyOn(console, 'log').mockImplementation();
-    console.log.mockClear();
+      jest.spyOn(console, 'log').mockImplementation();
+      console.log.mockClear();
 
-    printGrid(1);
+      printGrid(size);
 
-    expect(console.log).toHaveBeenCalledTimes(1);
-    expect(console.log).toHaveBeenCalledWith(' ');
-  });
-
-  it('prints a 2x2 grid when size = 2', () => {
-    expect.assertions(2);
-
-    jest.spyOn(console, 'log').mockImplementation();
-    console.log.mockClear();
-
-    printGrid(2);
-
-    expect(console.log).toHaveBeenCalledTimes(1);
-    expect(console.log).toHaveBeenCalledWith(` #
-# `);
-  });
-
-  it('prints a 3x3 grid when size = 3', () => {
-    expect.assertions(2);
-
-    jest.spyOn(console, 'log').mockImplementation();
-    console.log.mockClear();
-
-    printGrid(3);
-
-    expect(console.log).toHaveBeenCalledTimes(1);
-    expect(console.log).toHaveBeenCalledWith(` # 
-# #
- # `);
-  });
-
-  it('prints a 4x4 grid when size = 4', () => {
-    expect.assertions(2);
-
-    jest.spyOn(console, 'log').mockImplementation();
-    console.log.mockClear();
-
-    printGrid(4);
-
-    expect(console.log).toHaveBeenCalledTimes(1);
-    expect(console.log).toHaveBeenCalledWith(` # #
-# # 
- # #
-# # `);
-  });
-
-  it('prints a 5x5 grid when size = 5', () => {
-    expect.assertions(2);
-
-    jest.spyOn(console, 'log').mockImplementation();
-    console.log.mockClear();
-
-    printGrid(5);
-
-    expect(console.log).toHaveBeenCalledTimes(1);
-    expect(console.log).toHaveBeenCalledWith(` # # 
-# # #
- # # 
-# # #
- # # `);
-  });
-
-  it('prints a 6x6 grid when size = 6', () => {
-    expect.assertions(2);
-
-    jest.spyOn(console, 'log').mockImplementation();
-    console.log.mockClear();
-
-    printGrid(6);
-
-    expect(console.log).toHaveBeenCalledTimes(1);
-    expect(console.log).toHaveBeenCalledWith(` # # #
-# # # 
- # # #
-# # # 
- # # #
-# # # `);
-  });
-
-  it('prints a 6x6 grid when size = 7', () => {
-    expect.assertions(2);
-
-    jest.spyOn(console, 'log').mockImplementation();
-    console.log.mockClear();
-
-    printGrid(7);
-
-    expect(console.log).toHaveBeenCalledTimes(1);
-    expect(console.log).toHaveBeenCalledWith(` # # # 
-# # # #
- # # # 
-# # # #
- # # # 
-# # # #
- # # # `);
+      expect(console.log).toHaveBeenCalledTimes(1);
+      expect(console.log).toHaveBeenCalledWith(expected);
+    });
   });
 });
