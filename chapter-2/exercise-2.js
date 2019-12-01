b2e166adf5e2814c4b9545a81b35f9cfa87f27c0
Maya Vera <maya@mayavera.me>
2019-12-01 14:12:27 -0500

    made unit tests for chapter 2, exercise 2

--- chapter-2/exercise-2.js
+++ chapter-2/exercise-2.js
@@ -1,13 +1,19 @@
-for (let i = 1; i <= 100; i += 1) {
-  let buffer = '';
+function fizzbuzz() {
+  for (let i = 1; i <= 100; i += 1) {
+    let buffer = '';
 
-  if (i % 3 === 0) {
-    buffer += 'Fizz';
+    if (i % 3 === 0) {
+      buffer += 'Fizz';
+    }
+
+    if (i % 5 === 0) {
+      buffer += 'Buzz';
+    }
+
+    console.log(buffer === '' ? i : buffer);
   }
-
-  if (i % 5 === 0) {
-    buffer += 'Buzz';
-  }
-
-  console.log(buffer === '' ? i : buffer);
 }
+
+module.exports = {
+  fizzbuzz,
+};
