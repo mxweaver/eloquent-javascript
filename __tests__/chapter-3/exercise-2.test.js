fe8b32258337cdf7dcb0170854e3a65aaa0a8980
Maya Vera <maya@mayavera.me>
2019-12-03 23:59:56 -0500

    added more tests for chapter 3, exercise 2

--- __tests__/chapter-3/exercise-2.test.js
+++ __tests__/chapter-3/exercise-2.test.js
@@ -1,10 +1,25 @@
 const { isEven } = require('../../chapter-3/exercise-2');
 
 describe('isEven', () => {
+  it('thinks 0 is even', () => {
+    expect.assertions(1);
+    expect(isEven(0)).toStrictEqual(true);
+  });
+
+  it('thinks 1 is odd', () => {
+    expect.assertions(1);
+    expect(isEven(1)).toStrictEqual(false);
+  });
+
+  it('works for negative numbers', () => {
+    expect.assertions(2);
+    expect(isEven(-1)).toStrictEqual(false);
+    expect(isEven(-2)).toStrictEqual(true);
+  });
+
   [
     [50, true],
     [75, false],
-    [-1, false],
     [-20, true],
   ].forEach(([x, expected]) => {
     it(`isEven(${x}) = ${expected}`, () => {
